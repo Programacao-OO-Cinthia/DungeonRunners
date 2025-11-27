b4ea96f30b26e65f683bb8d15baed904319a9eca
Matheus Alves Passos <136806259+MatheusPas@users.noreply.github.com>
2025-11-27 12:01:12 -0300

    Update
    Removed unused fields from MembroGuilda class.

--- DungeonRunnersApp/app/src/main/java/Type/MembroGuilda.java
+++ DungeonRunnersApp/app/src/main/java/Type/MembroGuilda.java
@@ -1,19 +1,11 @@
 package Type;
 
 public class MembroGuilda extends User{
-    private String id;
-    private String nickname;
-    private int nivel;
-    private double kmTotal;
     private int xp;
     private String cargo;
 
     public MembroGuilda(String id, String nickname, int nivel, int idGuilda, double kmTotal, int xp, String cargo) {
         super(id, nickname, nivel, idGuilda, kmTotal);
-        this.id = id;
-        this.nickname = nickname;
-        this.nivel = nivel;
-        this.kmTotal = kmTotal;
         this.xp = xp;
         this.cargo = cargo;
     }
